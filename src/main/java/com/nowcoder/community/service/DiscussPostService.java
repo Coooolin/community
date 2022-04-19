4453e229ab18e9c778eb23e110a29e82dbaf56ea
Coooolin <2460776750@qq.com>
2022-04-20 00:27:17 +0800

    首次提交

---
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -0,0 +1,27 @@
+package com.nowcoder.community.service;
+
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DiscussPostService {
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    public List<DiscussPost> findDiscussPosts(int uerId, int offset, int limit) {
+        return discussPostMapper.selectDiscussPosts(uerId, offset, limit);
+    }
+
+
+    public int findDiscussPostRows(int userId) {
+        return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+
+
+}
